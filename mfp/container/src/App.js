1f55d0e420670496f39491cff375ec3e58a26e64
joaovictorrb <joaov.rbrum@gmail.com>
2022-03-02 22:43:41 -0300

    Almost done with mfp project

--- mfp/container/src/App.js
+++ mfp/container/src/App.js
@@ -1,22 +1,50 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import React, { lazy, Suspense, useState, useEffect } from 'react'
+import { Router, Route, Switch, Redirect } from 'react-router-dom'
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
-import MarketingService from './components/MarketingService'
+import { createBrowserHistory } from 'history'
+
+import Progress from './components/Progress'
 import Header from './components/Header'
+
+const Marketing = lazy(() => import('./components/MarketingService'))
+const Auth = lazy(() => import('./components/AuthService'))
+const Dashboard = lazy(() => import('./components/DashboardService'))
 
 const generateClassName = createGenerateClassName({
 	productionPrefix: 'co'
 })
 
+const history = createBrowserHistory()
+
 export default () => {
+	const [ isSignedIn, setIsSignedIn ] = useState(false)
+
+	useEffect(
+		() => {
+			if (isSignedIn) history.push('/dashboard')
+		},
+		[ isSignedIn ]
+	)
+
 	return (
-		<BrowserRouter>
+		<Router history={history}>
 			<StylesProvider generateClassName={generateClassName}>
 				<div>
-					<Header />
-					<MarketingService />
+					<Header onSignOut={() => setIsSignedIn(false)} isSignedIn={isSignedIn} />
+					<Suspense fallback={<Progress />}>
+						<Switch>
+							<Route path='/auth'>
+								<Auth onSignIn={() => setIsSignedIn(true)} />
+							</Route>
+							<Route path='/dashboard'>
+								{!isSignedIn && <Redirect to='/' />}
+								<Dashboard />
+							</Route>
+							<Route path='/' component={Marketing} />
+						</Switch>
+					</Suspense>
 				</div>
 			</StylesProvider>
-		</BrowserRouter>
+		</Router>
 	)
 }
